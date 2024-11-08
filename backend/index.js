5d384ba7fc54e87ec54f2f36a16c13d1e4219a52
IT22371904SLIIT <kavindu2002tcc@gmail.com>
2024-11-08 23:41:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,16 +5,16 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-app.use(cors({origin :true , credentials:true}));
+app.use(cors({ origin: true, credentials: true }));
 
 dbConnection();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",(req, res) =>res.send("Hello world!"));
-app.use("/api/stocks",routes);
+app.get("/", (req, res) => res.send("Hello world!"));
+app.use("/api/stocks", routes);
 
 const PORT = 4000;
 
-app.listen(PORT,()=>console.log('Server running on PORT ${PORT}'));
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
